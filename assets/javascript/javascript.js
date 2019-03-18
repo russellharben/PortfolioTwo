ce1d1bf24896fbeda011edf18b5f61df2e31a3e5
Russell H <russellharben@gmail.com>
2019-03-18 17:03:51 -0400

    typewriter text and text color

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,13 +1,12 @@
 var win = window;
 
-console.log("Test");
 
-$(win).on("resize", function(e){
+$(win).on("resize", function (e) {
     var windowSize = win.outerWidth;
     console.log(win.outerWidth);
     console.log("Window size" + " " + windowSize);
 
-    if(windowSize < 768){
+    if (windowSize < 768) {
         $("#navButton").removeClass("hidden");
         $("nav h4").css("margin-top", "-30px");
         e.stopPropagation();
@@ -20,4 +19,28 @@ $(win).on("resize", function(e){
 });
 
 
+
+var interval = setInterval(typeWriter, Math.floor(Math.random() * (200 - 100) + 100));
+var counter = 0;
+var span = $("<span>");
+$("#homeText").append(span);
+
+
+function typeWriter() {
+    var greeting = "Welcome! Thank you for checking out my page.";
+    var msg = greeting.split('');
+    var letter = msg[counter];
+    let str = msg;
+    console.log(str.length);
+
+    if (counter < str.length) {
+        span.append(letter).css("font-size","20px").css("font-weight","regular").css("color","#862d86");
+        counter++;
+        console.log("Counter = ", counter);
+    } else {
+        clearInterval(interval);
+    }
+}
+
+interval();
 
